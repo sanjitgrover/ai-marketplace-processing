47339931dfc770bffae15bb6dd367a281e31834c
sanjitgrover <sanjit.grover@gmail.com>
2023-06-28 18:12:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,25 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import os, platform
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 model = None
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Define the input data model
 class InputData(BaseModel):
     var1: float
